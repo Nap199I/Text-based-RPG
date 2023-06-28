399736ff09c860cfbf22fd64147c30be84392a6c
Nap199I <nathan_parry15@hotmail.com>
2023-06-28 13:32:14 +0000

    Add tests for countdown timer

--- assets/js/testing/countdown.test.js
+++ assets/js/testing/countdown.test.js
@@ -2,9 +2,8 @@
  * @jest-environment jsdom
  */
 
-const { test, expect } = require("@jest/globals");
-const { function1, function2, function3 } = require("../countdown");
-const { describe } = require("yargs");
+const { setInterval } = require("../countdown");
+jest.useFakeTimers();
 
 beforeAll(() => {
     let fs = require("fs");
@@ -15,6 +14,13 @@ beforeAll(() => {
 });
 
 describe("dom tests", () => {
-    test("span content should countdown");
-    expect(document.getElementById("countdown").innerHTML).toEqual("0");
+    test("span content should start at 10", () => {
+        expect(document.getElementById("countdown").innerHTML).toEqual("10");
+    });
+    // this test fails i am unable to advance the countdown timer but this is what i had
+    test("span content should end at 0", () => {
+        setInterval();
+        jest.advanceTimersByTime(10000);
+        expect(document.getElementById("countdown").innerHTML).toEqual("0");
+    });
 });
